eb2b7f4bc3c96629993ac36f134fb27b80aec0c5
Felix Fischer <felix.fischer.hh@gmail.com>
2026-02-24 00:56:42 +0100

    fix(auth): use signInWithRedirect on mobile devices to prevent Safari PWA block

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,25 @@
-import { GoogleAuthProvider, signInWithPopup, signOut, setPersistence, browserLocalPersistence } from 'https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js';
+import { 
+    GoogleAuthProvider, 
+    signInWithPopup, 
+    signInWithRedirect, 
+    signOut, 
+    setPersistence, 
+    browserLocalPersistence 
+} from 'https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js';
 
 const provider = new GoogleAuthProvider();
 
 export async function signInWithGoogle(auth) {
     await setPersistence(auth, browserLocalPersistence);
-    return signInWithPopup(auth, provider);
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+        return signInWithRedirect(auth, provider);
+    } else {
+        return signInWithPopup(auth, provider);
+    }
 }
 
 export async function signOutUser(auth) {
     return signOut(auth);
 }
-
